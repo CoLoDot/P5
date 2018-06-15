e16613b35417b1a5a52588812aa46ac22319558d
CoLoDot <CLG@pc8.home>
2018-06-15 11:46:57 +0200

    Udpate prototype : classes et méthodes

--- prototype.py
+++ prototype.py
@@ -9,86 +9,99 @@ import logging as lg
 
 lg.basicConfig(level=lg.DEBUG)
 
-# connect to database
+
+
 cnx = mysql.connector.connect(user='root', 
-		                      password='XXXXX', 
+		                      password='XXXXXX', 
 		                      host='localhost', 
 		                      database= 'OPENFOODFACTS', 
 		                      auth_plugin='mysql_native_password')
-# create a cursor 
-cur = cnx.cursor()
-
-# request to import data from OFF
 r = requests.get('https://fr.openfoodfacts.org/categorie/jus-d-orange/1.json')
-
-# return keys of OFF 
-if (r.status_code == requests.codes.ok):
-        print(r.headers['content-type'])
-commit_data = json.loads(r.text)
-
-# Table JSON
 table_JSON_page = r.json()
 page_size = table_JSON_page[u'page_size'] # return number of products by page
 count = table_JSON_page[u'count'] # return number of product by category
 total_page_number = int(count/page_size)+1 # return number of pages by category
 
+
 class Product:
 	"""Class Product : get products from OFF, send products to database """
 	def __init__(self): # constructor
 		self.products_data = [] # create empty list for data from OFF
-		print("hello init")
 
-	def get_products_from_OFF(self, products_data): # function to get data with requests' module
-		for self.page in range(0, 1):
-			self.rpage = requests.get('https://fr.openfoodfacts.org/categorie/jus-d-orange/' + str(page+1) + '.json')
+	def get_products_from_OFF(self): # function to get data with requests' module
+		for self.page in range(0, 10):
+			self.rpage = requests.get('https://fr.openfoodfacts.org/categorie/jus-d-orange/' 
+										+ str(self.page+1) + '.json')
 			self.table_JSON_page = self.rpage.json()
 			self.products_by_page = self.table_JSON_page[u'products']
 
-			for products in self.products_by_page:
-				self.name = products['product_name']
-				self.brand = products['brands']
-				self.nutriscore = products['nutrition_grades_tags']
-				self.url = products['url']
-				self.id_produit = '1'
+			for self.products in self.products_by_page:
+				self.name = self.products['product_name']
+				self.brand = self.products['brands']
+				self.nutriscore = self.products['nutrition_grades_tags']
+				self.url = self.products['url']
+				self.products_data.append([self.name, 
+											self.brand, 
+											self.nutriscore, 
+											self.url])
+		#print("Hello i am get_products_from_OFF")
+		#print(self.products_data)
 
-			self.products_data.append([self.name, self.brand, self.nutriscore, self.url, self.id_produit])
-			print("Hello get_products_from_OFF")
-
-	def send_products_to_db(self, products_data): # function to send products to databse
-		self.query = ("INSERT INTO Orangejuice (nom, marque, nutriscore, url, id_produit) VALUES (%(nom)s, %(marque)s, %(nutriscore)s, %(url)s, %(id_produit)s)")
+	def send_products_to_db(self): # function to send products to databse
+		cursor = cnx.cursor()
 		self.data_for_db = {
-		'nom': self.products_data[12][0],
-		'marque': self.products_data[12][1],
-		'nutriscore': 'A', # test substitut nutriscore
-		'url': self.products_data[12][3],
-		'id_produit': '1',
+		'nom': self.products_data[14][0],
+		'marque': self.products_data[14][1],
+		'nutriscore': self.products_data[14][2][0], 
+		'url': self.products_data[14][3],
 		}
-		print("hello send_products_to_db")
+		
+		self.query = ("INSERT INTO Jus_orange "
+						"(`id`, `produit_id`, `nom`, `marque`, `nutriscore`, `url`)"
+						" VALUES (NULL, '1', %(nom)s, %(marque)s, %(nutriscore)s, %(url)s);")
+		
+		cursor.execute(self.query, self.data_for_db)
+		#print(self.data_for_db)
+		#print("Hello i am send_products_to_db")
 
+def main():
 
-print('Quel aliment souhaitez-vous remplacer ? (1) ou retrouver mes aliments substitués (2)')
-user_input = input('Indiquez le chiffre correspondant à votre souhait : ')
+	cur = cnx.cursor()
+	X = Product()
+	X.get_products_from_OFF()
+	sending = X.send_products_to_db()
+	
+	
 
-if user_input == '1': 
-	print('Affichage de la table de catégories de produit')
-	c = cnx.cursor()
-	c.execute("SELECT * FROM Orangejuice")
-	for data in c:
-		print(data)
-	user_input_1 = input('Si vous souhaitez afficher les produits avec un nutriscore supérieur à C tapez appuyez sur 1 :')
-	if user_input_1 == '1':
+	print('1 - Quel aliment souhaitez-vous remplacer ?')
+	print('2 - Retrouver mes aliments substitués')
+	user_input = input('Indiquez le chiffre correspondant à votre souhait : ')
+
+	if user_input == '1': 
+		print('Affichage de la table de catégories de produit')
+		c = cnx.cursor()
+		c.execute("SELECT * FROM Produit")
 		for data in c:
-			c.execute("SELECT `ID_AUTO`, `nom`, `marque`, `nutriscore`, `url`, `id_produit` FROM `Orangejuice` WHERE 'nutriscore'='A'")
-		print(data)
-	c.close()
+			print(data)
+		
+		user_input_1 = input('Si vous souhaitez afficher les produits Jus d''orange tapez 1 : ')
+		if user_input_1 == '1':
+			cursor = cnx.cursor()
+			cursor.execute("SELECT * FROM Jus_orange")
+			for data_2 in c:
+				print(data_2)
+		c.close()
 
-if user_input == '2':
-	print('Accès à vos produits sauvegardés')
+	if user_input == '2':
+		print('Accès à vos produits sauvegardés...')
 
-if user_input > '2':
-	print('Votre choix de correspond à aucune option')
+	if user_input > '2':
+		print('Votre choix ne correspond à aucune option !')
 
-# Execution of database
-#cur.execute(query, data_for_db)
-cnx.commit()
-cur.close()
+	#cur.execute(X.send_products_to_db(products_data, query, data_for_db))
+	cnx.commit()
+	cur.close()
+
+
+if __name__ == '__main__': # Encapsulation of main function
+    main()
