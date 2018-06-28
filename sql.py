4316dec9c77b934142791ab42b2d802858e0503d
CoLoDot <CLG@macbook-pro-de-constance-louise.home>
2018-06-28 16:44:31 +0200

    Add sql param file

--- sql.py
+++ sql.py
@@ -5,7 +5,7 @@ import mysql.connector
 from mysql.connector import errorcode
 
 cnx = mysql.connector.connect(user='root', 
-		                      password='XXXXXX', 
+		                      password='XXXXXXX', 
 		                      host='localhost', 
 		                      database= 'OPENFOODFACTS', 
 		                      auth_plugin='mysql_native_password')
