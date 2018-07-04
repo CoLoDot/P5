ee160ea2cd3afbcfdadc3d0e59a2ccaa7dac2df5
CoLoDot <CLG@macbook-pro-de-constance-louise.home>
2018-07-04 12:21:27 +0200

    add file to create tables in db

---
+++ sql.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+from mysql.connector import errorcode
+
+
+cnx = mysql.connector.connect(user='root', 
+		                      password='XXXXXXXX', 
+		                      host='localhost', 
+		                      database= 'OPENFOODFACTS', 
+		                      auth_plugin='mysql_native_password')
+
+cursor = cnx.cursor()
+
+try:
+	create_table = "CREATE TABLE `Category` ("\
+					"`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,"\
+					" `Produit` varchar(40) NOT NULL,"\
+					" PRIMARY KEY (`id`)"\
+					") ENGINE=InnoDB"
+	cursor.execute(create_table)
+	cnx.commit()
+
+	create_table_1 = "CREATE TABLE `Product` ("\
+					" `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,"\
+					" `produit_id` smallint(5) unsigned NOT NULL,"\
+					"`nom` text NOT NULL,"\
+					"`marque` text NOT NULL,"\
+					"`nutriscore` text NOT NULL,"\
+					"`url` text NOT NULL,"\
+					 "PRIMARY KEY (`id`),"\
+					 "KEY `fk_produit_id` (`produit_id`),"\
+					 "CONSTRAINT `fk_produit_id` FOREIGN KEY (`produit_id`) REFERENCES Category(id)"\
+					") ENGINE=InnoDB"
+	cursor.execute(create_table_1)
+	cnx.commit()
+
+	create_table_2 = "CREATE TABLE `Saved` ("\
+					 "`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,"\
+					 "`produit_id` smallint(5) unsigned NOT NULL,"\
+					 "`nom` text NOT NULL,"\
+					 "`marque` text NOT NULL,"\
+					 "`nutriscore` text NOT NULL,"\
+					 "`url` text NOT NULL,"\
+					 "PRIMARY KEY (`id`)"\
+					") ENGINE=InnoDB"
+	cursor.execute(create_table_2)
+	cnx.commit()
+
+except:
+	print("Les tables existent déjà dans la base de données OPENFOODFACTS.")
+else:
+	print("Bienvenue !")
+finally:
+	cnx.close()
